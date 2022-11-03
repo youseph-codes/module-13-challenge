af2dce5b12b4e4bd54ddcc53b299d32a295045a2
Yousopp <yousephagueye@gmail.com>
2022-11-03 00:09:46 -0400

    get all products in product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,6 +7,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    include: [{
+      model: Category,
+      attributes: [
+        'category_name'
+      ],
+    },
+    {
+      model: Tag,
+      attributes: [
+        'tag_name'
+      ],
+    }
+    ]
+  })
+  .then((dbProductData) => res.json(dbProductData))
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
