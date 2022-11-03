64deb81ac91a1907519b8abdc76090c841b7a39c
Yousopp <yousephagueye@gmail.com>
2022-11-03 00:30:48 -0400

    created single delete function by id value

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -155,6 +155,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({ message: 'No product with that id '});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
