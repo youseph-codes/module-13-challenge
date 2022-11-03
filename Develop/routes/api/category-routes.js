c7037381b8935ce8b487aa2d9c7795f7563d375d
Yousopp <yousephagueye@gmail.com>
2022-11-02 23:31:49 -0400

    found one category by id value

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -25,8 +25,34 @@ router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
     Category.findOne({
-      where: {}
+      where: {
+        id: req.params.id,
+      },
+      attributes: [
+        'id',
+        'category_name'
+      ], include: [{
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ],
+      }]
     })
+    .then((dbCategoryData) => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'There is no category with that id'});
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
