28e088079f68a1864c6e9cb934b7cef1ae6eb8db
Yousopp <yousephagueye@gmail.com>
2022-11-03 21:15:21 -0400

    created update tag feature

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -46,6 +46,21 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: "Can't find a category with this id" });
+      return;
+    }
+    res.json(dbCategoryData);
+}).catch(err => {
+  console.log(err);
+  res.status(500).json(err);
+});
 });
 
 router.delete('/:id', (req, res) => {
