cb4fdba2cd18b4005d049ff6b8f581368ee738cc
Yousopp <yousephagueye@gmail.com>
2022-11-03 21:04:13 -0400

    created an add new tag function

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -34,6 +34,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
